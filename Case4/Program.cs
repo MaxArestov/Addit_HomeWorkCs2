58d83fd44e939c29c92fe1193de5727a1eef995b
maxim arestov <aresmax1993@gmail.com>
2022-10-17 20:03:53 +0300

    Completed Case 4

---
+++ Case4/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Написать программу которая заполняет массив рандомными числами. Количество рандомных чисел пользователь задает из консоли. Вывести заполненый массив в консоль.
+
+Пример: Вввели 3 => 7, 4, 5 */
+Console.Clear();
+Console.Write("Введите количество чисел в массиве: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] A = new int[N];
+int index = 0;
+while(index < N)
+{
+    A[index] = new Random().Next(1, 10001);
+    index++;
+}
+foreach (var item in A) 
+{
+    Console.WriteLine(item);
+}
