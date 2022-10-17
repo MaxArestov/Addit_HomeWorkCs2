402bbdd18a20572c0d9e0592a7fc61eb8b6b9bd2
maxim arestov <aresmax1993@gmail.com>
2022-10-17 19:05:04 +0300

    Completed Case 2

--- Case1/Program.cs
+++ Case1/Program.cs
@@ -1,29 +1,29 @@
 ﻿/* 1) Написать программу которая находит сумму 4 чисел. Числа вводятся пользователем из консоли.
  Если пользователь ввел не число необходимо вывести пользователю сообщение "Введены некорректные данные".*/
 Console.Clear();
-Console.WriteLine("Введите поочередно 4 числа, разделяя их пробелом.");
-string a = Console.ReadLine();
+Console.WriteLine("Введите поочередно 4 числа, разделяя их клавишей Enter.");
+var a = Console.ReadLine();
 bool check = StringDigit(a);
 if (check == false)
 {
     Console.WriteLine("Введены некорректные данные");
     return;
 }
-string b = Console.ReadLine();
+var b = Console.ReadLine();
 check = StringDigit(b);
 if (check == false)
 {
     Console.WriteLine("Введены некорректные данные");
     return;
 }
-string c = Console.ReadLine();
+var c = Console.ReadLine();
 check = StringDigit(c);
 if (check == false)
 {
     Console.WriteLine("Введены некорректные данные");
     return;
 }
-string d = Console.ReadLine();
+var d = Console.ReadLine();
 check = StringDigit(d);
 if (check == false)
 {
---
+++ Case2/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 2) Написать программу которая считывает из консоли 2 числа A и B и определяет делится ли число А на B без остатка.
+ Результатом выполнения программы должно быть вывода сообщения пользователю true или false.
+true - если делится.
+false - если не делится.*/
+Console.Clear();
+Console.WriteLine("Введите два числа, разделяя их клавишей Enter.");
+Console.Write("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+bool check = CheckDev(A, B);
+if(check == true) Console.WriteLine("true");
+else Console.WriteLine("false");
+
+
+
+bool CheckDev(int first, int second)
+{
+    if((first % second) == 0 || (B % A) == 0)
+    {
+        return true;
+    }
+    else return false;
+}
