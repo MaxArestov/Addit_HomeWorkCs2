1619b989df9678a9aab554d6950380ba6057c2f3
maxim arestov <aresmax1993@gmail.com>
2022-10-17 19:10:39 +0300

    Completed Case 3

---
+++ Case3/Program.cs
@@ -0,0 +1,8 @@
+﻿/* 3) Написать программу которая сравнивает две строки. Пользователь вводит две строки.
+ Если строки равны то выводится сообщение true если нет то false.*/
+ Console.Clear();
+ Console.WriteLine("Введите дважды текст, разделяя клавишей Enter.");
+ string A = Console.ReadLine();
+ string B = Console.ReadLine();
+ if(A == B) Console.WriteLine("true");
+ else Console.WriteLine("false");
