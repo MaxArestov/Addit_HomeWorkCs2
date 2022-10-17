758eba6956b9e4f24b89a91ba5e41ebb67ece7b8
maxim arestov <aresmax1993@gmail.com>
2022-10-17 22:21:31 +0300

    Crashed on Case 5. Waiting tomorrow...

---
+++ Case5/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Написать программу которая сравнивает два массива. На вход пользователь заполняет два массива.
+ Если каждый элемент массива равен каждому элементу второго массива вывести true, если хотя бы один не равен то вывести false. */
+Console.Clear();
+int[] A = new int[5];
+int[] B = new int[5];
+foreach (var item in A)
+{
+    Console.Write("Введите число: ");
+    Int.Parse(Console.ReadLine());
+}
+foreach (var item in B)
+{
+    Console.Write("Введите число: ");
+    Int.Parse(Console.ReadLine());
+}
+if (A[5] == B[5])
+{
+    Console.Write("True");
+}
+else Console.Write("False");
