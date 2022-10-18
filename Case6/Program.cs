b1c58274aa334d0701b8db166c76605e0de1c2d9
maxim arestov <aresmax1993@gmail.com>
2022-10-18 18:46:06 +0300

    Complete Case 6

---
+++ Case6/Program.cs
@@ -0,0 +1,50 @@
+﻿// 6) Написать программу которая сравнивает два массива и выводит индексы элементов в которых числа не равны.
+Console.Clear();
+int N = 4;
+int[] A = new int[N];
+int[] B = new int[N];
+FillArray(A);
+FillArray(B);
+PrintArray(A);
+PrintArray(B);
+
+int c = 0;
+Console.WriteLine("В массиве не совпадают цифры на позициях: ");
+while (c < N)
+{
+    if (A[c] != B[c])
+    {
+        Console.WriteLine(c);
+    }
+    c++;
+}
+
+
+
+
+
+void PrintArray(int[] mass)
+{
+    int line = mass.Length;
+    int count = 0;
+    Console.Write("Элементы массива: ");
+    while (count < line)
+    {
+        Console.Write(mass[count]);
+        count++;
+    }
+}
+
+
+
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        array[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
