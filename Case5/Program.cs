4ea127b84dddd598a074ced6a131d0712340f307
maxim arestov <aresmax1993@gmail.com>
2022-10-18 17:05:02 +0300

    Completed Case 5... oh

--- Case5/Program.cs
+++ Case5/Program.cs
@@ -1,20 +1,40 @@
 ﻿/* Написать программу которая сравнивает два массива. На вход пользователь заполняет два массива.
  Если каждый элемент массива равен каждому элементу второго массива вывести true, если хотя бы один не равен то вывести false. */
 Console.Clear();
-int[] A = new int[5];
-int[] B = new int[5];
-foreach (var item in A)
+int N = 5;
+int[] A = new int[N];
+int[] B = new int[N];
+Console.WriteLine("Введите числа, относящиеся к массиву A, разделяя их клавишей Enter: ");
+FillArray(A);
+Console.WriteLine("Введите числа, относящиеся к массиву B, разделяя их клавишей Enter: ");
+FillArray(B);
+PrintArray(A);
+PrintArray(B);
+Console.WriteLine(A.SequenceEqual(B));
+
+
+
+
+
+void PrintArray(int[] mass)
 {
-    Console.Write("Введите число: ");
-    Int.Parse(Console.ReadLine());
+    int line = mass.Length;
+    int count = 0;
+    Console.WriteLine("Элементы массива: ");
+    while (count < line)
+    {
+        Console.WriteLine(mass[count]);
+        count++;
+    }
 }
-foreach (var item in B)
+
+void FillArray(int[] array)
 {
-    Console.Write("Введите число: ");
-    Int.Parse(Console.ReadLine());
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        array[index] = Convert.ToInt32(Console.ReadLine());
+        index++;
+    }
 }
-if (A[5] == B[5])
-{
-    Console.Write("True");
-}
-else Console.Write("False");
