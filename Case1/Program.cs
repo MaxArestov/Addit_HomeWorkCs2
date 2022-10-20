aeec85758d6c7a3f1a5a096306c2ea2dde61d9ca
maxim arestov <aresmax1993@gmail.com>
2022-10-20 22:59:02 +0300

    "Fixed" case 1

--- Case1/Program.cs
+++ Case1/Program.cs
@@ -1,51 +1,49 @@
 ﻿/* 1) Написать программу которая находит сумму 4 чисел. Числа вводятся пользователем из консоли.
  Если пользователь ввел не число необходимо вывести пользователю сообщение "Введены некорректные данные".*/
 Console.Clear();
-Console.WriteLine("Введите поочередно 4 числа, разделяя их клавишей Enter.");
-var a = Console.ReadLine();
-bool check = StringDigit(a);
-if (check == false)
+int a = 0;
+bool check = true;
+while (check)
 {
-    Console.WriteLine("Введены некорректные данные");
-    return;
-}
-var b = Console.ReadLine();
-check = StringDigit(b);
-if (check == false)
-{
-    Console.WriteLine("Введены некорректные данные");
-    return;
-}
-var c = Console.ReadLine();
-check = StringDigit(c);
-if (check == false)
-{
-    Console.WriteLine("Введены некорректные данные");
-    return;
-}
-var d = Console.ReadLine();
-check = StringDigit(d);
-if (check == false)
-{
-    Console.WriteLine("Введены некорректные данные");
-    return;
-}
-int Sum = Convert.ToInt32(a) + Convert.ToInt32(b) + Convert.ToInt32(c) + Convert.ToInt32(d);
-Console.WriteLine(Sum);
-
-
-
-
-
-
-bool StringDigit(string number)
-{
-    foreach (var item in number)
+    Console.Write("Plesase enter number: ");
+    if (Int32.TryParse(Console.ReadLine(), out a))
     {
-        if (!char.IsDigit(item))
-        {
-            return false;
-        }
+        check = false;
     }
-    return true;
+    else Console.WriteLine("Введены некорректные данные");
 }
+int b = 0;
+check = true;
+while (check)
+{
+    Console.Write("Plesase enter number: ");
+    if (Int32.TryParse(Console.ReadLine(), out b))
+    {
+        check = false;
+    }
+    else Console.WriteLine("Введены некорректные данные");
+}
+int c = 0;
+check = true;
+while (check)
+{
+    Console.Write("Plesase enter number: ");
+    if (Int32.TryParse(Console.ReadLine(), out c))
+    {
+        check = false;
+    }
+    else Console.WriteLine("Введены некорректные данные");
+}
+int d = 0;
+check = true;
+while (check)
+{
+    Console.Write("Plesase enter number: ");
+    if (Int32.TryParse(Console.ReadLine(), out d))
+    {
+        check = false;
+    }
+    else Console.WriteLine("Введены некорректные данные");
+}
+int Sum = a + b + c + d;
+Console.WriteLine(Sum);
