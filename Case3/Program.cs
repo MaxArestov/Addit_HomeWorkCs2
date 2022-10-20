a449a11efdf5eff925aa74fd935f4a5d638ba2c2
maxim arestov <aresmax1993@gmail.com>
2022-10-20 23:05:29 +0300

    Fixed Case 3

--- Case3/Program.cs
+++ Case3/Program.cs
@@ -1,8 +1,24 @@
 ﻿/* 3) Написать программу которая сравнивает две строки. Пользователь вводит две строки.
  Если строки равны то выводится сообщение true если нет то false.*/
  Console.Clear();
- Console.WriteLine("Введите дважды текст, разделяя клавишей Enter.");
- string A = Console.ReadLine();
- string B = Console.ReadLine();
- if(A == B) Console.WriteLine("true");
- else Console.WriteLine("false");
+ Console.WriteLine("Введите текст, разделяя клавишей Enter.");
+ string a = Console.ReadLine();
+ string b = Console.ReadLine();
+Dif(a, b);
+
+
+
+
+
+
+
+
+void Dif(string x, string y)
+{
+    bool check = x == y;
+    if (check)
+    {
+        Console.WriteLine("true"); 
+    }
+    else Console.WriteLine("false");
+}
